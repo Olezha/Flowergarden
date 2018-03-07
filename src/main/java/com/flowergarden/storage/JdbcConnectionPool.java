8557fa9762f84c70819be4b32309d09ca975d1c6
Oleh Shklyar <pio_ua@ukr.net>
2018-03-07 23:15:03 +0200

    wrap sql connection

--- src/main/java/com/flowergarden/storage/JdbcConnectionPool.java
+++ src/main/java/com/flowergarden/storage/JdbcConnectionPool.java
@@ -1,8 +1,19 @@
 package com.flowergarden.storage;
 
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcConnectionPool {
+
+    private List<JdbcConnectionFromPool> connections = new ArrayList<>();
+    private int size;
+
+    public JdbcConnectionPool() throws SQLException {
+        for (int i = 0; i < size; i++)
+            connections.add(new JdbcConnectionFromPool(DriverManager.getConnection("jdbc:sqlite:./flowergarden.db")));
+    }
 
     public JdbcConnectionFromPool getConnection() throws SQLException {
         return null;
