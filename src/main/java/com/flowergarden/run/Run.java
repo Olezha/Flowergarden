4f357b873bd1004996cfabb91516cd6105ca11e5
Oleh Shklyar <pio_ua@ukr.net>
2018-03-07 10:58:52 +0200

    lesson 3

--- src/main/java/com/flowergarden/run/Run.java
+++ src/main/java/com/flowergarden/run/Run.java
@@ -1,7 +1,21 @@
 package com.flowergarden.run;
 
+import java.sql.*;
+
 public class Run {
 
-    public static void main(String[] args) {
+    private static final String SELECT_FLOWERS = "select * from flower";
+
+    public static void main(String[] args) throws SQLException {
+        Run run = new Run();
+        try (Statement statement = run.getConnection().createStatement()) {
+            ResultSet flowersResultSet = statement.executeQuery(SELECT_FLOWERS);
+            while (flowersResultSet.next())
+                System.out.println(flowersResultSet.getInt("id") + " " + flowersResultSet.getString("name"));
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:sqlite:./flowergarden.db");
     }
 }
