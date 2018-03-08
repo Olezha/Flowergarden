a78171e89e487a5fcbf2664f8037d5331681c2e8
Oleh Shklyar <pio_ua@ukr.net>
2018-03-08 11:36:55 +0200

    properties

--- src/main/java/com/flowergarden/storage/JdbcConnectionPool.java
+++ src/main/java/com/flowergarden/storage/JdbcConnectionPool.java
@@ -1,21 +1,56 @@
 package com.flowergarden.storage;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class JdbcConnectionPool {
 
+    public static void main(String[] args) throws SQLException {
+        JdbcConnectionPool jdbcConnectionPool = new JdbcConnectionPool();
+    }
+
     private List<JdbcConnectionFromPool> connections = new ArrayList<>();
-    private int size;
+    private String datasourceUrl = "jdbc:sqlite:./flowergarden.db";
+    private int connectionPoolSize = 10;
 
     public JdbcConnectionPool() throws SQLException {
-        for (int i = 0; i < size; i++)
-            connections.add(new JdbcConnectionFromPool(DriverManager.getConnection("jdbc:sqlite:./flowergarden.db")));
+        loadProperties("application.properties");
+
+        for (int i = 0; i < connectionPoolSize; i++)
+            connections.add(
+                    new JdbcConnectionFromPool(
+                            DriverManager.getConnection(datasourceUrl)));
     }
 
     public JdbcConnectionFromPool getConnection() throws SQLException {
         return null;
+    }
+
+    private void loadProperties(String propertiesFileName) {
+        try (InputStream propertiesStream =
+                     Thread.currentThread().getContextClassLoader()
+                             .getResourceAsStream(propertiesFileName)) {
+            Properties properties = new Properties();
+            properties.load(propertiesStream);
+
+            if (properties.containsKey("datasource.url"))
+                System.out.println("properties.contains(\"datasource.url\")");
+            if (properties.containsKey("connection.pool.size"))
+                try {
+                    connectionPoolSize = Integer.parseInt(properties.getProperty("connection.pool.size"));
+                } catch (NumberFormatException e) {
+                    System.out.println("Parameter \"connection.pool.size\" has a wrong format");
+                }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
