d3f20252d6def28d0926f1fd5e3b6f297dfbe0bc
Oleh Shklyar <pio_ua@ukr.net>
2018-03-09 19:08:44 +0200

    #7.4

--- src/test/java/com/flowergarden/bouquet/BouquetTest.java
+++ src/test/java/com/flowergarden/bouquet/BouquetTest.java
@@ -13,26 +13,31 @@ import java.util.Collection;
 
 import static org.mockito.Mockito.mock;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 public class BouquetTest {
 
     private Bouquet<Flower> bouquet;
+    private Rose rose = new Rose();
+    private Chamomile chamomile = new Chamomile(0, 0, mock(BigDecimal.class), mock(FreshnessInteger.class));
 
     @Before
     public void initBouquet() {
         bouquet = new MarriedBouquet();
-        bouquet.addFlower(new Rose());
-        bouquet.addFlower(new Chamomile(0, 0, mock(BigDecimal.class), mock(FreshnessInteger.class)));
+        bouquet.addFlower(rose);
+        bouquet.addFlower(chamomile);
     }
 
     @Test
     public void getPriceTest() {
         BigDecimal price = bouquet.getPrice();
-        Assert.assertEquals(new BigDecimal(120), price);
+        assertEquals(new BigDecimal(120), price);
     }
 
     @Test
     public void searchFlowersByLengthTest() {
         Collection<Flower> flowers = bouquet.searchFlowersByLength(0, 0);
-        Assert.assertEquals(flowers.isEmpty(), false);
+        assertThat(bouquet.getFlowers(), hasItems(rose, chamomile));
     }
 }
