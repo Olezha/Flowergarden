ef1bca7beff0d65aef7cdce9715b151268c565d0
Oleh <pio_ua@ukr.net>
2018-03-12 13:17:59 +0200

    restore from backup

--- src/test/java/com/flowergarden/sql/RestoreFromBackupDbRule.java
+++ src/test/java/com/flowergarden/sql/RestoreFromBackupDbRule.java
@@ -6,18 +6,14 @@ import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
-public class BackupRule extends ExternalResource {
+public class RestoreFromBackupDbRule extends ExternalResource {
 
     public static void main(String[] args) throws IOException, SQLException {
-        Properties properties = new Properties();
-        properties.load(ClassLoader.getSystemResourceAsStream("application.properties"));
-
         try (Statement statement = DriverManager
-                .getConnection(properties.getProperty("datasource.url"))
+                .getConnection("jdbc:sqlite:")
                 .createStatement()) {
-            statement.executeUpdate("backup to backup.db");
+            statement.executeUpdate("restore from backup.db");
         }
     }
 }
