4383c85df0cfcb7800fbc98a3e38754b4a8d803f
Oleh Shklyar <pio_ua@ukr.net>
2018-03-21 22:15:29 +0200

    Spring Boot DBCP2

--- src/main/java/com/flowergarden/FlowergardenApplication.java
+++ src/main/java/com/flowergarden/FlowergardenApplication.java
@@ -2,27 +2,17 @@ package com.flowergarden;
 
 import com.flowergarden.service.BouquetService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
 
 @EnableCaching
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = FlywayAutoConfiguration.class)
 public class FlowergardenApplication implements CommandLineRunner {
 
     private BouquetService bouquetService;
@@ -39,38 +29,6 @@ public class FlowergardenApplication implements CommandLineRunner {
     @Override
     public void run(String... strings) {
         System.out.println("Bouquet id1 price is " + bouquetService.getBouquetPrice(1));
-    }
-}
-
-@Configuration
-class FlywayConfiguration {
-
-    public FlywayConfiguration(@Value("${datasource.url}") String datasourceUrl) {
-        Flyway flyway = new Flyway();
-        flyway.setDataSource(datasourceUrl, null, null);
-        flyway.migrate();
-    }
-}
-
-@Configuration
-class ContextConfiguration {
-
-    @Value("${datasource.url}")
-    private String datasourceUrl;
-
-    @Value("${connection.pool.size.init}")
-    private int connectionPoolInitialSize;
-
-    @Value("${connection.pool.size.max}")
-    private int connectionPoolMaxTotal;
-
-    @Bean(destroyMethod = "")
-    DataSource dataSource() {
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(datasourceUrl);
-        basicDataSource.setInitialSize(connectionPoolInitialSize);
-        basicDataSource.setMaxTotal(connectionPoolMaxTotal);
-        return basicDataSource;
     }
 }
 
