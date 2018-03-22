4c0ece2632b7a351073ea59fa85aa0fb1a7bc582
Oleh <pio_ua@ukr.net>
2018-03-22 17:21:22 +0200

    fix tests db url

--- src/test/java/com/flowergarden/repository/FlowerRepositoryTest.java
+++ src/test/java/com/flowergarden/repository/FlowerRepositoryTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
